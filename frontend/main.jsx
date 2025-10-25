b8f91d3d66611dde5bc30bde15f7e2806399781b
Fenet Gizaw <simbani011@gmail.com>
2025-10-25 14:11:33 -0700

    Update main.jsx

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import WeatherApp from './WeatherApp.jsx'
 import './styles/App.css'
 
+
 const root = createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
